a11af5c940201ade754924a69f857dd24e271700
Dinesh <dineshghadge876@gmail.com>
2023-05-17 12:58:15 +0530

    add testcase

--- testng project/src/testngpro/testingtitles.java
+++ testng project/src/testngpro/testingtitles.java
@@ -1,7 +1,9 @@
 package testngpro;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -57,6 +59,22 @@ public class testingtitles {
 		} else {
 			System.out.println("failed test");
 		}
+
+	}
+
+	@FindBy(xpath = "//img[@alt='Facebook']")
+	public static WebElement fblogo;
+
+	@Test
+	public static void tr() {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\driver\\chromedriver.exe");
+		//
+		d = new ChromeDriver();
+
+		d.get("https://www.facebook.com");
+
+		System.out.println(fblogo.isDisplayed());
 
 	}
 
